cddb2871d9f85fe9ad29e27c788add53d1744de5
sanket-kogekar <kogekar@proton.me>
2023-03-01 19:14:07 +0530

    0.4% fee to admin.

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -76,7 +76,7 @@ const CreateCampaign = () => {
 
         <div className="w-full flex justify-start items-center p-4 bg-[#10ac84] h-[120px] rounded-[10px]">
           <img src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
-          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">You will get 99.8% of the raised amount</h4>
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">You will get 99.6% of the raised amount</h4>
         </div>
 
         <div className="flex flex-wrap gap-[40px]">
